f556c04f0c3ddc8c2f3818022c747fa9327cc78f
Sergey Polishchuk <mayorserega@gmail.com>
2014-05-23 14:51:16 +0300

    initial commit

---
+++ test/test_api_first/core_test.clj
@@ -0,0 +1,7 @@
+(ns test-api-first.core-test
+  (:require [clojure.test :refer :all]
+            [test-api-first.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
