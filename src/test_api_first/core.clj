f0c27ee05e50898241f6a60d88f15a3d1217a860
Sergey Polishchuk <mayorserega@gmail.com>
2014-06-02 16:38:44 +0300

    start requests in different threads

--- src/test_api_first/core.clj
+++ src/test_api_first/core.clj
@@ -4,7 +4,7 @@
         [compojure.core])
   (:require [org.httpkit.client :as http]
             [test_api_first.query-generator :as gen]
-            [test_api_first.config :as conf]))
+            [test-api-first.config :as conf]))
 
 (defn send-it [url data]
   (http/get (str url) {:json data}))
@@ -15,13 +15,23 @@
     (doseq [url urls] (send-it url data)
       (println url))))
 
+(defn number-of-requests [number]
+  (dotimes [n number] (make-request)))
+
 (def number-of-threads
   5)
 
+(defn start-thread [fn]
+  (.start (Thread. fn)))
+
+(defn main-method []
+  (dotimes [n number-of-threads]
+    (future (start-thread #(number-of-requests 10)))))
+
 ; need to fix
 ; doesn't work at server, only in REPL
-(defn app []
-  (time (doall (map deref (doall (for [i (range number-of-threads)] (future (make-request))))))))
+; (defn app []
+;   (time (doall (map deref (doall (for [i (range number-of-threads)] (future (make-request))))))))
 
-(defn -main []
-  (run-jetty app {:port 8080}))
+; (defn -main []
+;   (run-jetty app {:port 8080}))
